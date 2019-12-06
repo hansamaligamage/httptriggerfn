5fc6494d64f2d0c64ec02c50e6b37aece9c732a0
hansamaligamage <hansamali.gamage@gmail.com>
2019-12-06 11:54:28 +0530

    changes in azure pipeline build

--- httptriggerfunction/Function1.cs
+++ httptriggerfunction/Function1.cs
@@ -26,7 +26,7 @@ namespace httptriggerfunction
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name} How are the new deployment changes ")
+                ? (ActionResult)new OkObjectResult($"Hello, {name} How are the new deployment changes with Azure pipelines")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
